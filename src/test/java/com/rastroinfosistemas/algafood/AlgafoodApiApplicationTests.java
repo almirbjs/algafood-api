7e5c083bf96af91af0ba630413d9588a99ee3c7a
almirbjs <almir.ricardobjs@gmail.com>
2022-01-26 20:18:26 -0300

    Upload pastas

---
+++ src/test/java/com/rastroinfosistemas/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.rastroinfosistemas.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+			
+	}
+
+}
