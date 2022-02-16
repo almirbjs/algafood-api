ad518e7cda539c1f05107c6896efc8a66a5ddd4e
Almir <almir.ricardobjs@gmail.com>
2022-02-16 12:40:16 -0300

    Criando um controller com Spring MVC

--- src/main/java/com/rastroinfosistemas/algafood/Controller.java
+++ src/main/java/com/rastroinfosistemas/algafood/Controller.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @org.springframework.stereotype.Controller
 public class Controller {
 	
-	//@getMapping informa o caminho pra chegar na resposta
-	//@ ResponseBody solicita a devolução da resposta para uma requisição
+	/*@getMapping informa o caminho pra chegar na resposta
+	@ ResponseBody solicita a devolução da resposta para uma requisição
+	Para acessar a pagina na maquina local utilizamos: http://localhost:8080/hello
+	*/
 	@GetMapping("/hello")
 	@ResponseBody
 	private String hello() {
