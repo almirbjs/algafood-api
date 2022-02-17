bc06cd2ce309518ec48579bc1f1f0e39b389b023
Almir <almir.ricardobjs@gmail.com>
2022-02-17 16:46:42 -0300

    2.12 Injeção de dependências.

--- src/main/java/com/rastroinfosistemas/algafood/Controller.java
+++ src/main/java/com/rastroinfosistemas/algafood/Controller.java
@@ -3,10 +3,23 @@ package com.rastroinfosistemas.algafood;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-//@Controller responsavel por receber requisicoes Web
+import com.rastroinfosistemas.algafood.di.modelo.Cliente;
+import com.rastroinfosistemas.algafood.di.services.AtivacaoClienteService;
+
+
+//TODO @Controller responsavel por receber requisicoes Web
 @org.springframework.stereotype.Controller
 public class Controller {
+	private AtivacaoClienteService ativacaoClienteService;
 	
+	
+	public Controller(AtivacaoClienteService ativacaoClienteService) {
+		super();
+		this.ativacaoClienteService = ativacaoClienteService;
+		System.out.println("Controller" + ativacaoClienteService);
+	}
+
+
 	/*@getMapping informa o caminho pra chegar na resposta
 	@ ResponseBody solicita a devolução da resposta para uma requisição
 	Para acessar a pagina na maquina local utilizamos: http://localhost:8080/hello
@@ -14,6 +27,8 @@ public class Controller {
 	@GetMapping("/hello")
 	@ResponseBody
 	private String hello() {
+		Cliente maria = new Cliente("Maria", "jo@gmail.com", "32356487", true);
+		ativacaoClienteService.ativar(maria);
 		return "Hello";
 		
 
